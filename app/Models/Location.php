7ec38a81b6f6ae26060cd6e012a4e2a133479e84
Tarek Hesham <tarekghost6@gmail.com>
2024-09-05 20:11:23 +0300

    Add Location in job xD

---
+++ app/Models/Location.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Location extends Model
+{
+    use HasFactory;
+    protected $table = "locations";
+
+    public function job(): BelongsTo
+    {
+        return $this->belongsTo(Job::class, 'location_id');
+    }
+}
