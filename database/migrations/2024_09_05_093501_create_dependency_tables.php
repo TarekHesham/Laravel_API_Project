7ee5fe6a0d409bf313f0dd52b6cd47def38d515a
Tarek ElFarmawy <37149220+TarekHesham@users.noreply.github.com>
2024-09-05 13:41:15 +0300

    Add all migrations tables (#1)

---
+++ database/migrations/2024_09_05_093501_create_dependency_tables.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('benefits', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('skills');
+        Schema::dropIfExists('benefits');
+        Schema::dropIfExists('locations');
+    }
+};
