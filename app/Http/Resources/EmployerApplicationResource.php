2a892e06a4f6dc22cd503868b79ead672cd98a73
Tarek Hesham <tarekghost6@gmail.com>
2024-09-09 12:37:30 +0300

    Show applications apply on job

---
+++ app/Http/Resources/EmployerApplicationResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\Users\UserResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployerApplicationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->status,
+            'candidate' => new UserResource($this->candidate),
+            'cv' => $this->type == 'cv' ? asset("cvs/{$this->cv->cv}") : null,
+        ];
+    }
+}
